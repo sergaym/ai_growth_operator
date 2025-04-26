e5c9330b832f0e8ad8b53de10a05e711f2af6921
sergaym <sergioayala.contacto@gmail.com>
2025-04-26 15:14:10 +0200

    fix (middleware): update route exclusion pattern for static files
    This commit modifies the middleware configuration to refine the route exclusion pattern, ensuring that requests for static files are correctly handled. The adjustment enhances the routing logic by explicitly excluding the necessary static assets, improving the overall functionality of the middleware.

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -47,6 +47,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * - public files (public directory)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|public).*)',
+    '/((?!_next/static|_next/image|favicon.ico|public).*)',
   ],
 }; 
