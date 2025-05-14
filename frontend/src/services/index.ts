16063a00e37298e42cf9ef846b53ff618f98d4d6
sergaym <sergioayala.contacto@gmail.com>
2025-05-14 07:16:35 +0200

    refactor(services): update service exports to include api-utils
    This commit modifies the service index file to remove exports for individual services and instead export all functionalities from the newly introduced `api-utils` module. This change streamlines the service exports and centralizes API utility functions, enhancing maintainability and organization within the codebase.

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -2,7 +2,4 @@
  * Service index file that exports all API services
  */
 
-export * from './text-to-image-service';
-export * from './text-to-speech-service';
-export * from './image-to-video-service';
-export * from './lipsync-service'; 
+export * from './api-utils'; 
